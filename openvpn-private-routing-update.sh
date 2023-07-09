d8ed33cb0a60f6d34ea5ef38bd042bf4c780d688
CuseTheJuice <93330138+CuseTheJuice@users.noreply.github.com>
2023-07-09 06:31:40 -0400

    Update openvpn-private-routing-update.sh
    Skip unresolved domains from config entry

--- openvpn-private-routing-update.sh
+++ openvpn-private-routing-update.sh
@@ -8,9 +8,12 @@ NUM=0
 while read DOMAIN; do
 	
 	IPADDRESS=`ping -c1 $DOMAIN | head -n1 | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}'`
-	echo "$NUM $DOMAIN $IPADDRESS"
-	/usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.private_network.${NUM}" --value "${IPADDRESS}/32" ConfigPut
-	NUM=`expr $NUM + 1`
+	if [ "$IPADDRESS" != "" ]
+ 	then
+ 		echo "$NUM $DOMAIN $IPADDRESS"
+		/usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.private_network.${NUM}" --value "${IPADDRESS}/32" ConfigPut
+	fi
+ 	NUM=`expr $NUM + 1`
 
 done < /usr/local/openvpn_as/scripts/vpn-route-domains.txt
 
